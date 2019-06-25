000f68c525a1737109dd4d1030154a83c5ffe26a
lwpwyf <1012312786@qq.com>
2019-06-26 01:08:22 +0800

    mockclass complte ui

---
+++ mockclass_talk/mockclass_talk/src/pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div class="home">
+            <!-- 首页搜索框 -->
+            <div class="home_search_box">
+                <div class="home_search">毕设 React 面试</div>
+            </div>      
+             <!-- 首页导航栏 -->
+            <div class="home_nav">
+               <van-tabs animated swipeable>
+                   <!-- 首页推荐分页 -->
+                    <van-tab title="推荐">
+                        推荐1111
+                    </van-tab>
+                    <!-- 首页实战分页 -->
+                    <van-tab title="实战">
+                        实战
+                    </van-tab>
+                </van-tabs>   
+            </div>   
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'index',
+    data (){
+        return {
+            active: 1
+        }
+    },
+    methods: {
+        onChange(event) {
+        wx.showToast({
+        title: `切换到标签 ${event.detail.index + 1}`,
+        icon: 'none'
+        });
+        } 
+    }
+}
+</script>
+
+<style scoped>
+    .home_search_box{
+        display: flex;
+        justify-content: center;
+    }
+    .home_search{
+        border: 1px solid #d9dde1;
+        border-radius: 100rpx;
+        height: 69rpx;
+        width: 660rpx;
+        text-align: center;
+        line-height: 69rpx;
+        color: #d9dde1;
+        font-size: 30rpx;
+    }
+</style>
---
+++ mockclass_talk/mockclass_talk/src/pages/mine/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        LOGS123456
+        <Button>111111</Button>
+        <van-button type="danger">危险按钮</van-button>
+    </div>
+</template>
+
+<script>
+import { Button } from 'vant';
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
