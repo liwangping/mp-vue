4a8dbf27d647ab4182937032854e9df3db9457ea
lwpwyf <1012312786@qq.com>
2019-06-26 01:02:30 +0800

    mockclass complte ui

--- mockclass1/mockclass1/src/pages/mine/index.vue
+++ mockclass1/mockclass1/src/pages/mine/index.vue
@@ -1,8 +1,44 @@
 <template>
     <div>
-        LOGS123456
-        <!-- <van-button type="danger">危险按钮</van-button> -->
-    </div>
+       <div class="mine_header">
+           <div class="person_image">
+               <img src="../../../static/images/mine_image.png" alt="">
+           </div>
+           <div class="person_more">
+                <div class="person_name">
+                    <strong>慕UI0338532</strong>
+                </div>
+                <div class="person_detail">
+                    <span class="title">学习时长</span>
+                    <span class="content">98小时41分</span>
+                    <span class="title">经验</span>
+                    <span class="content">3644</span>
+                </div>
+           </div>      
+       </div>
+       <div class="headline"></div>
+       <div class="mine_lists">
+           <div class="list">
+               <div class="icon"></div>
+               <div class="title">我的实战</div>
+           </div>        
+           <div class="list">
+               <div class="icon"></div>
+               <div class="title">我的订单</div>
+           </div>        
+           <div class="list">
+               <div class="icon"></div>
+               <div class="title">优惠券</div>
+           </div>        
+           <div class="list">
+               <div class="icon"></div>
+               <div class="title">
+                   客服/反馈
+                    <span class="time">周一至周五 09:00-19:00</span>
+               </div>
+           </div>        
+       </div>
+    </div> 
 </template>
 
 <script>
@@ -12,6 +48,64 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.mine_header{
+    display: flex;
+}
+.person_image{
+    width: 62px;
+    height: 62px;
+    margin-left: 20px;
+}
+.person_image img{
+    width: 100%;
+    height: 100%;
+}
+.person_name{
+    font-size: 20px;
+}
+.person_more{
+    padding: 10px;
+}
+.person_detail{
+  
+}
+.person_detail .title{
+    font-size: 11px;
+    line-height: 100%;
+    color: #9ea2a6;
+    margin-right: 6px;
+}
+.person_detail .content{
+    font-size: 11px;
+    line-height: 100%;
+    color: #70757b;
+    margin-right: 20px;
+}
+.headline{
+    height: 9px;
+    background-color: #f3f5f7;
+    border-top: 2px solid #dce2e7;
+}
+.list{
+    height: 60px;
+    display: flex;
+}
+.list .icon{
+    width: 60px;
+}
+.list .title{
+    width: 100%;
+    border-bottom: 1px solid #e6e8eb;
+    font-size: 14px;
+    color:#50595e;
+    line-height: 60px;
+}
+.time{
+    color: #c4c8cb;
+    float: right;
+    margin-right: 31px;
+    font-size: 13px;
+}
 
 </style>
--- mockclass1/mockclass1/src/pages/shopping/index.vue
+++ mockclass1/mockclass1/src/pages/shopping/index.vue
@@ -3,41 +3,42 @@
         <div class="header">
             <view class="van-hairline--bottom"></view>
         </div>
-        <!-- <van-checkbox-group :value="result" @change="onChange">
-            <van-cell-group >
-                <van-cell
-                v-for="item in list"
-                :key="item"
-                :title="['复选框'+item]"
-                value-class="value-class"
-                clickable
-                data-index="index"
-                @click="toggle"
-                >
-                <van-checkbox catch:tap="noop" :class="['checkboxes-'+index]" :name="item" />
-                </van-cell>
-            </van-cell-group>
-        </van-checkbox-group> -->
         <van-checkbox-group :value="result" @change="onChange">
             <van-checkbox
                 v-for="item in list"
                 :key="item"
                 :name="item"
+                custom-class = "custom_class"
+                icon-class="icon_class1"
             >
                 <div class="Goods">
                     <div class="Goods_image"> 
                         <img src="../../../static/images/class_1.png" alt="">
                     </div>
                     <div class="Good_detail">
-                        <div class="Good_title"></div>
+                        <div class="Good_title">
+                            <view class="van-multi-ellipsis--l2">Node.js从零开发Web Server博客项目 前端晋升全栈工程师必备</view>
+                        </div>
                         <div class="Good_Situation">
-                            <div class="Good_price"></div>
-                            <div class="Good_Category"></div>
+                            <div class="Good_price">￥288.00</div>
+                            <div class="Good_Category">实战</div>
                         </div>
                     </div>
                 </div>
             </van-checkbox>
         </van-checkbox-group>
+        <van-submit-bar
+        :price="3050"
+        button-text="提交订单"
+        @submit="onClickButton"
+        bar-class="bar_class"
+        >
+        <van-tag type="primary">
+            <van-radio-group :value="radio" @change="all_onChange">
+                <van-radio name="1">全选</van-radio>
+            </van-radio-group>
+        </van-tag>
+        </van-submit-bar>
     </div>
 </template>
 
@@ -46,7 +47,8 @@ export default {
  data() {
     return{
         list: ['a', 'b', 'c'],
-        result: ['a', 'b']
+        result: ['a', 'b'],
+        radio: '1'
     }
   },
   computed: {
@@ -64,22 +66,67 @@ export default {
         checkbox.toggle();
     },
 
-    noop() {}
+    noop() {},
+    all_onChange(event){
+        this.setData({
+        checked: event.detail
+    });
+    }
   }
   
   
 }
 </script>
-
 <style>
-.value-class {
-  flex: none !important;
+.van-hairline--bottom{
+    padding-bottom: 20px;
 }
+.custom_class{
+    margin-top: 9px;
+}
+.icon_class1{
+    height: 50px !important;
+}
+.value-class {
+    flex: none !important;
+}
+.van-checkbox__icon-wrap{
+    /* margin-top: 18px !important;
+     */
+    padding-top: 27px !important;
+    padding-left: 8px !important;
+}
+.bar_class{
+    padding-left: 17px;
+}
+</style>
+
+<style scoped>
+
 .Goods{
     height: 72px;
+    display: flex;
 }
 .Goods_image img{
     width: 108px;
     height: 72px;
+}
+.Good_detail{
+    margin: 11px 27px 0 14px;
+}
+.Good_detail .Good_title{
+    font-size: 16px;
+}
+.Good_Situation{
+    display: flex;
+}
+.Good_price{
+    font-size: 11px;
+    color: #f34646;
+}
+.Good_Category{
+    margin-left: 12px;
+    font-size: 11px;
+    color: #a7abae;
 }
 </style>
